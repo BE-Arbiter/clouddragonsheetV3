37cdd0e92c2e2cdbd3f6f6258d74c145c482d859
BuildTools <unconfigured@null.spigotmc.org>
2024-03-14 00:36:29 +0100

    Backend First Stub

---
+++ src/test/java/be/arbiter/clouddragonsheet/CloudDragonSheetV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.arbiter.clouddragonsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDragonSheetV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
